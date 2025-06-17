490c69e2b1d735e9eede73335f0dc3e173376003
Nira-Bohara <nirubohara520@gmail.com>
2025-06-17 03:55:45 +0000

    learn about datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,38 @@
+"use strict" // treate all JS code as newer version
+
+//alert(3+4) // we are using node js, not browser
+
+console.log("niur"); console.log(5+4); //This way of writing code in JavaScript doesn't seem good
+ 
+/*
+console.log("niur");
+
+console.log(5+4);
+*/ // this way is good for js
+
+/*
+for document read usign ecma(tc39.es)
+"Sometimes, you can use MDN documentation, but for the original documentation, use ECMAScript."
+*/
+
+let name = "niru"
+age =54
+let isLoggedIn = false
+let state = null
+
+/*
+//datatypes
+- number= 2 to the  power 53
+- bigint
+string= ""
+-boolean=true/false
+-null =atandalone value
+-undefined
+-symbol=unique
+*/
+
+//object
+
+//check type (usign typeof)
+console.log(typeof niru);
+
